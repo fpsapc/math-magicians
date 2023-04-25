d913df56069d2e635c7da3ebc86e7fd184521ea7
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-04-26 01:14:20 +0500

    Add events

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
 export default function Buttons() {
+  const [currentState, setState] = useState({ total: 0, next: null, operation: null });
+
+  const mouseClick = (e) => {
+    setState((currentState) => calculate(currentState, e.target.textContent));
+  };
   return (
     <>
-      <div className="keypad">
-        <button type="button" className="singleButton">AC</button>
-        <button type="button" className="singleButton">+/-</button>
-        <button type="button" className="singleButton">%</button>
-        <button type="button" className="singleButton">/</button>
-        <button type="button" className="singleButton">7</button>
-        <button type="button" className="singleButton">8</button>
-        <button type="button" className="singleButton">9</button>
-        <button type="button" className="singleButton">X</button>
-        <button type="button" className="singleButton">4</button>
-        <button type="button" className="singleButton">5</button>
-        <button type="button" className="singleButton">6</button>
-        <button type="button" className="singleButton">-</button>
-        <button type="button" className="singleButton">1</button>
-        <button type="button" className="singleButton">2</button>
-        <button type="button" className="singleButton">3</button>
-        <button type="button" className="singleButton">+</button>
-        <button type="button" className="doubleButton">0</button>
-        <button type="button" className="singleButton">.</button>
-        <button type="button" className="singleButton">=</button>
+      <div className="container">
+        <div className="resultBar">
+          {currentState.total}
+          {currentState.operation}
+          {currentState.next}
+        </div>
+        <div className="keypad">
+          <button type="button" onClick={mouseClick} className="singleButton">AC</button>
+          <button type="button" onClick={mouseClick} className="singleButton">+/-</button>
+          <button type="button" onClick={mouseClick} className="singleButton">%</button>
+          <button type="button" onClick={mouseClick} className="singleButton">÷</button>
+          <button type="button" onClick={mouseClick} className="singleButton">7</button>
+          <button type="button" onClick={mouseClick} className="singleButton">8</button>
+          <button type="button" onClick={mouseClick} className="singleButton">9</button>
+          <button type="button" onClick={mouseClick} className="singleButton">x</button>
+          <button type="button" onClick={mouseClick} className="singleButton">4</button>
+          <button type="button" onClick={mouseClick} className="singleButton">5</button>
+          <button type="button" onClick={mouseClick} className="singleButton">6</button>
+          <button type="button" onClick={mouseClick} className="singleButton">-</button>
+          <button type="button" onClick={mouseClick} className="singleButton">1</button>
+          <button type="button" onClick={mouseClick} className="singleButton">2</button>
+          <button type="button" onClick={mouseClick} className="singleButton">3</button>
+          <button type="button" onClick={mouseClick} className="singleButton">+</button>
+          <button type="button" onClick={mouseClick} className="doubleButton">0</button>
+          <button type="button" onClick={mouseClick} className="singleButton">.</button>
+          <button type="button" onClick={mouseClick} className="singleButton">=</button>
+        </div>
       </div>
     </>
   );
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,10 +4,7 @@ import Buttons from './Buttons';
 export default function Calculator() {
   return (
     <>
-      <div className="container">
-        <input type="text" className="resultBar" placeholder="0" />
-        <Buttons />
-      </div>
+      <Buttons />
     </>
   );
 }
