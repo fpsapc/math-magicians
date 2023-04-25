8ee27b6b07b01db5b64fea722f37d49b44cbbfc2
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-04-25 17:49:26 +0500

    Make all changes as required by reviewer

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,31 +1,12 @@
 import React from 'react';
+import Buttons from './Buttons';
 
 export default function Calculator() {
   return (
     <>
       <div className="container">
         <input type="text" className="resultBar" placeholder="0" />
-        <div className="keypad">
-          <button type="button" className="singleButton">AC</button>
-          <button type="button" className="singleButton">+/-</button>
-          <button type="button" className="singleButton">%</button>
-          <button type="button" className="singleButton">/</button>
-          <button type="button" className="singleButton">7</button>
-          <button type="button" className="singleButton">8</button>
-          <button type="button" className="singleButton">9</button>
-          <button type="button" className="singleButton">X</button>
-          <button type="button" className="singleButton">4</button>
-          <button type="button" className="singleButton">5</button>
-          <button type="button" className="singleButton">6</button>
-          <button type="button" className="singleButton">-</button>
-          <button type="button" className="singleButton">1</button>
-          <button type="button" className="singleButton">2</button>
-          <button type="button" className="singleButton">3</button>
-          <button type="button" className="singleButton">+</button>
-          <button type="button" className="doubleButton">0</button>
-          <button type="button" className="singleButton">.</button>
-          <button type="button" className="singleButton">=</button>
-        </div>
+        <Buttons />
       </div>
     </>
   );
