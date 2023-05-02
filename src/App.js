fcff1b820928be8cf2a3e5324896c332b73513ef
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-05-03 00:34:05 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import {
-  BrowserRouter as Router, Routes, Route,
-} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
@@ -13,13 +11,11 @@ function App() {
   return (
     <>
       <Layout />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />}> </Route>
-          <Route path="calculator" element={<Calculator />}> </Route>
-          <Route path="quote" element={<Quote />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Home />}> </Route>
+        <Route path="calculator" element={<Calculator />}> </Route>
+        <Route path="quote" element={<Quote />} />
+      </Routes>
     </>
   );
 }
