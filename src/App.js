5a21571980e047c4e97388741df47ffd8e1bfd07
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-05-02 23:11:56 +0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
+import Home from './components/Home';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <>
-      <Calculator />
-      <Quote />
+      <Layout />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}> </Route>
+          <Route path="calculator" element={<Calculator />}> </Route>
+          <Route path="quote" element={<Quote />} />
+        </Routes>
+      </Router>
     </>
   );
 }
