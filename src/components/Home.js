97c507a8e6dce048d8c46f8bea24681c8d22516c
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-05-02 23:12:26 +0500

    Create Home component

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h2>Welcome to our Page!</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores soluta maxime, nostrum?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores soluta maxime, nostrum?</p>
+  </div>
+);
+
+export default Home;
